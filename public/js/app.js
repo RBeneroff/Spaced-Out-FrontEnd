eaae6830e7f955f564a683d46e3f74e40d7fdad8
Rachel Beneroff <rbbeneroff@gmail.com>
2016-11-03 12:48:58 -0400

    addNewFavorites

--- public/js/app.js
+++ public/js/app.js
@@ -50,7 +50,37 @@
       $state.go('home', {url: '/'})
     }
 
+    $http.get(`${rootUrl}/users`)
+    .then(function(response) {
+      console.log(response);
+      self.users = response.data.users;
+    })
 
-  });
+    this.addToFavorites = function(font, user_id) {
+      // self.user = response.data.user;
+    //  var newFont = font.replace
+    console.log('user-id', user_id);
+      var newFont = font.replace(/['"]+/g, '').split(',');
+      var fontObj = {
+        fontName: newFont[0],
+        fontSerif: newFont[1].replace(/\s/g, ''),
+        fontOrigin: newFont[2].replace(/\s/g, ''),
+      };
+      console.log(fontObj);
+      console.log('font', newFont);
+      return $http({
+        url: `${rootUrl}/users/${user_id}/fonts`,
+        method: 'POST',
+        data: {font: newFont}
+      })
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(err) {
+        console.log(err);
+      })
+    }
+
+  }); //controller closure
 
 })()
